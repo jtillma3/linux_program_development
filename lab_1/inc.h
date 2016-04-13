907081087c1a569fa3690dea1c1ee3b6b7746146
Hawke <jtillma3@gmail.com>
2016-04-12 22:27:48 -0700

    Adding lab_1 files and make script

---
+++ lab_1/inc.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#ifndef INC_H
+#define INC_H
+	void foo(void);
+
+#endif
---
+++ lab_1/main.c
@@ -0,0 +1,11 @@
+#include "inc.h"
+
+#include <stdio.h>
+
+int main(void){
+
+
+printf("Hello,world - from main.\n");
+foo();
+return 0;
+}
